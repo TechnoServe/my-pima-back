19e650982b80cc452d2a0fccfe6e354dc41c602a
Mugenga <rockyyves@gmail.com>
2023-12-14 09:23:30 +0200

    sync bug

--- src/resolvers/participants.resolvers.mjs
+++ src/resolvers/participants.resolvers.mjs
@@ -881,7 +881,7 @@ const ParticipantsResolvers = {
 
       // Perform the initial query
       let result = await sf_conn.query(
-        "SELECT Id, Name, Create_In_CommCare__c FROM Participant__c WHERE Project__c = '" +
+        "SELECT Id, Name, Create_In_CommCare__c, Resend_to_OpenFN__c FROM Participant__c WHERE Project__c = '" +
           project.project_name +
           "'"
       );
@@ -899,6 +899,7 @@ const ParticipantsResolvers = {
       participants = participants.map((participant) => {
         return {
           Id: participant.Id,
+          Resend_to_OpenFN__c: false,
           Create_In_CommCare__c: true, // Assuming Create_In_CommCare__c is a checkbox
         };
       });
